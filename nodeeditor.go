7aa8297978956ac6eb2dfdd130d8c18cdead37e2
Tony Horrobin <tony.horrobin@gmail.com>
2024-04-07 18:23:40 +0100

    Add rudimentary NodeView. - still has issues with positioning of ports

--- nodeeditor.go
+++ nodeeditor.go
@@ -1,35 +1,39 @@
 package main
 
 import (
-	"fmt"
-
-	"fyne.io/fyne/v2"
+	"NodeEditorFyne/nodeeditor"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/data/binding"
-	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	myApp := app.New()
-	myWindow := myApp.NewWindow("List Data")
+	myWindow := myApp.NewWindow("Node Editor")
 
-	data := binding.BindStringList(
-		&[]string{"Item 1", "Item 2", "Item 3"},
-	)
-
-	list := widget.NewListWithData(data,
-		func() fyne.CanvasObject {
-			return widget.NewLabel("template")
-		},
-		func(i binding.DataItem, o fyne.CanvasObject) {
-			o.(*widget.Label).Bind(i.(binding.String))
-		})
-
-	add := widget.NewButton("Append", func() {
-		val := fmt.Sprintf("Item %d", data.Length()+1)
-		data.Append(val)
-	})
-	myWindow.SetContent(container.NewBorder(nil, add, nil, nil, list))
+	//node := nodeeditor.NewNodeModel(0, "test", nodeeditor.CAT_NONE)
+	input := nodeeditor.NewPortModel(0, "foo", nodeeditor.TYPE_DOUBLE, nodeeditor.DIR_IN)
+	nodeView := nodeeditor.NewNodeView()
+	nodeView.SetTitle("test")
+	nodeView.AddPort(input)
+	output := nodeeditor.NewPortModel(1, "bar", nodeeditor.TYPE_DOUBLE, nodeeditor.DIR_OUT)
+	nodeView.AddPort(output)
+	internal := nodeeditor.NewPortModel(2, "int", nodeeditor.TYPE_BOOL, nodeeditor.DIR_INTERNAL)
+	nodeView.AddPort(internal)
+	//data := binding.BindStringList(
+	//	&[]string{"Item 1", "Item 2", "Item 3"},
+	//)
+	//
+	//list := widget.NewListWithData(data,
+	//	func() fyne.CanvasObject {
+	//		return widget.NewLabel("template")
+	//	},
+	//	func(i binding.DataItem, o fyne.CanvasObject) {
+	//		o.(*widget.Label).Bind(i.(binding.String))
+	//	})
+	//
+	//add := widget.NewButton("Append", func() {
+	//	val := fmt.Sprintf("Item %d", data.Length()+1)
+	//	data.Append(val)
+	//})
+	myWindow.SetContent(nodeView.GetGrid())
 	myWindow.ShowAndRun()
 }
--- nodeeditor/nodeview.go
+++ nodeeditor/nodeview.go
@@ -2,9 +2,11 @@ package nodeeditor
 
 import (
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+	"golang.org/x/image/colornames"
 )
 
 type NodeView struct {
@@ -17,8 +19,52 @@ func NewNodeView() *NodeView {
 	instance := new(NodeView)
 	instance.obj = new(fyne.Container)
 	instance.grid = container.New(layout.NewVBoxLayout())
-	instance.title = widget.NewLabel("")
 
+	instance.title = widget.NewLabel("")
 	instance.grid.Add(instance.title)
+
 	return instance
+}
+
+func (self *NodeView) GetObj() fyne.CanvasObject {
+	return self.obj
+}
+
+func (self *NodeView) GetGrid() *fyne.Container {
+	return self.grid
+}
+
+func (self *NodeView) SetTitle(title string) {
+	self.title.SetText(title)
+	self.title.Alignment = fyne.TextAlignCenter
+}
+
+func (self *NodeView) AddPort(port *PortModel) {
+	parent := container.NewHBox()
+	switch port.Direction {
+	case DIR_IN:
+		dirChild := canvas.NewCircle(colornames.Red)
+		dirChild.Resize(fyne.NewSize(10, 10))
+		noLayout := container.NewWithoutLayout(dirChild)
+		nameChild := widget.NewLabel(port.Name)
+		parent.Add(noLayout)
+		parent.Add(nameChild)
+		//parent.Add(layout.NewSpacer())
+	case DIR_OUT:
+		dirChild := canvas.NewCircle(colornames.Green)
+		dirChild.Resize(fyne.NewSize(10, 10))
+		noLayout := container.NewWithoutLayout(dirChild)
+		nameChild := widget.NewLabel(port.Name)
+		parent.Add(layout.NewSpacer())
+		parent.Add(nameChild)
+		parent.Add(noLayout)
+	case DIR_INTERNAL:
+		nameChild := widget.NewLabel(port.Name)
+		parent.Add(layout.NewSpacer())
+		parent.Add(nameChild)
+		parent.Add(layout.NewSpacer())
+	default:
+		// Do nothing.
+	}
+	self.grid.Add(parent)
 }
